ddb5c670e673f0d0a200c73b29023869757e06b1
Semyon <125258352+SSUHOY@users.noreply.github.com>
2023-12-13 13:55:00 +0700

    adds parallax and mousewheel slider effect

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,15 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+import { Swiper } from "swiper";
+import { Parallax, Mousewheel } from "swiper/modules";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  Swiper.use([Parallax, Mousewheel]);
 
-	// Custom JS
-
-})
+  const swiperIMG = new Swiper(".slider-img", {
+    loop: false,
+    speed: 2400,
+    parallax: true,
+    mousewheel: {
+      invert: false,
+    },
+  });
+});
