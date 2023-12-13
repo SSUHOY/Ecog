f5e55edd97926c6283481ec4a11c70336776f192
Semyon <125258352+SSUHOY@users.noreply.github.com>
2023-12-13 18:11:31 +0700

    adds scrollbar, pagination, swiper-btns

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,49 @@
 import { Swiper } from "swiper";
-import { Parallax, Mousewheel } from "swiper/modules";
+import {
+  Parallax,
+  Mousewheel,
+  Controller,
+  Pagination,
+  Scrollbar,
+  Navigation,
+} from "swiper/modules";
 
 document.addEventListener("DOMContentLoaded", () => {
-  Swiper.use([Parallax, Mousewheel]);
+  Swiper.use([
+    Parallax,
+    Mousewheel,
+    Controller,
+    Pagination,
+    Scrollbar,
+    Navigation,
+  ]);
 
   const swiperIMG = new Swiper(".slider-img", {
     loop: false,
     speed: 2400,
     parallax: true,
+  });
+
+  const swiperText = new Swiper(".slider-text", {
+    loop: false,
+    speed: 2400,
     mousewheel: {
       invert: false,
     },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
+
+  swiperIMG.controller.control = swiperText;
+  swiperText.controller.control = swiperIMG;
 });
